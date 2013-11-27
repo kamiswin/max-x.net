b50514e100ac6cf3aac0ddd4d3e9ceee71de8982
Max <apple@appletekiMacBook-Pro.local>
2013-11-27 14:47:17 +0800

    update

--- crawler/start.sh
+++ crawler/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /usr/local/bin/virtualenvwrapper.sh
-workon pythoner
+workon max-xn.com
 
 uwsgi --socket 127.0.0.1:8077 --chdir /root/maxfile/max-x.net/crawler -H /root/.virtualenvs/max-xn.com/ --module=crawler.wsgi --processes 4 --threads 2 --daemonize /root/maxfile/max-x.net/crawler/uwsgi.log
