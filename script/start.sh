fe6a2157c2ffa8a43c8dbace6678e0095e8b1038
Max <seraph0017@hotmail.com>
2014-03-04 11:56:17 +0800

    update

--- script/start.sh
+++ script/start.sh
@@ -5,4 +5,4 @@ export ENV=$1
 source /usr/local/bin/virtualenvwrapper.sh
 workon max-xn.com
 
-uwsgi --socket 127.0.0.1:8077 --chdir /root/maxfile/max-x.net/crawler -H /root/.virtualenvs/max-xn.com/ --module=crawler.wsgi --processes 4 --threads 2 --daemonize /root/maxfile/max-x.net/crawler/uwsgi.log
+uwsgi --socket 127.0.0.1:8077 --chdir /root/Projects/max-x.net/crawler -H /root/.virtualenvs/max-xn.com/ --module=crawler.wsgi --processes 4 --threads 2 --daemonize /root/Projects/max-x.net/crawler/uwsgi.log
